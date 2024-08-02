29a53c188a9d4090fc3217758fb33ada36a95f4c
Bikash <medhi01011997@gmail.com>
2024-08-02 13:34:00 +0530

    Mod2 : 7.  Conditional Rendering using && Operator

--- 2. Intro To JSX/index.js
+++ 2. Intro To JSX/index.js
@@ -3,15 +3,11 @@ const App = () => {
   const password = "demo";
   return (
     <>
-      {email == "xyz@gmail.com" ? (
-        password == "demo" ? (
-          <h1>User Exists</h1>
-        ) : (
-          <h1>Wrong Password</h1>
-        )
-      ) : (
-        <h1>Create a new Account</h1>
+      {email == "xyz@gmail.com" && password == "demo" && <h1>User Exists</h1>}
+      {email == "xyz@gmail.com" && password != "demo" && (
+        <h1>Wrong Password</h1>
       )}
+      {email != "xyz@gmail.com" && <h1>Create New Account</h1>}
     </>
   );
 };
