c30025b1a0c48ee2c62d59f2dff49a7b3554cbc8
Bikash <medhi01011997@gmail.com>
2024-08-02 11:48:11 +0530

    4a. Creating elements and rendering using React

--- 1. Getting Started with React/react.js
+++ 1. Getting Started with React/react.js
@@ -1,2 +1,10 @@
-const heading = React.createElement('h1',{className:"heading"},"Hello")
-ReactDOM.createRoot(document.getElementById("root")).render(heading)
+const heading = React.createElement(
+    "h1",
+    { className: "heading" },
+    "Hello Bikash"
+  );
+
+const para = React.createElement('p',{className:"para"},"Welcome to the session")
+const btn = React.createElement("button",{className:"btn"},"Click Me Now")
+const div = React.createElement('div',{className:"App",children:[heading,para,btn]}) 
+ReactDOM.createRoot(document.getElementById("root")).render(div);
