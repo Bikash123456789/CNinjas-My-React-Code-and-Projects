8753e73914e3144c1e31293f07741dd40e489f71
Bikash <medhi01011997@gmail.com>
2024-08-02 13:28:34 +0530

    Mod2: Ternary Operator

--- 2. Intro To JSX/index.js
+++ 2. Intro To JSX/index.js
@@ -1,34 +1,17 @@
 const App = () => {
+  const email = "xyz@gmail.com";
+  const password = "demo";
   return (
     <>
-      <table border="1">
-        <caption>This is the caption</caption>
-        <thead>
-          <tr>
-            <th>Col1</th>
-            <th>Col2</th>
-            <th>Col3</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td>Bikash</td>
-            <td>27</td>
-            <td>Assam</td>
-          </tr>
-          <tr>
-            <td>Bikash</td>
-            <td>27</td>
-            <td>Assam</td>
-          </tr>
-          <tr>
-            <td>Bikash</td>
-            <td>27</td>
-            <td>Assam</td>
-          </tr>
-        </tbody>
-        <tfoot>Table Footer</tfoot>
-      </table>
+      {email == "xyz@gmail.com" ? (
+        password == "demo" ? (
+          <h1>User Exists</h1>
+        ) : (
+          <h1>Wrong Password</h1>
+        )
+      ) : (
+        <h1>Create a new Account</h1>
+      )}
     </>
   );
 };
