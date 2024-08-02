6f19350d5990a83818b50d5f5778fa96febea203
Bikash <medhi01011997@gmail.com>
2024-08-02 11:14:21 +0530

    3. Creating DOM Elements using JS Code

--- 1. Getting Started with React/index.js
+++ 1. Getting Started with React/index.js
@@ -1,1 +1,19 @@
-document.write("Hello from External JS file")
+const div = document.createElement("div");
+
+const heading = document.createElement("h1");
+heading.textContent = "Hello"
+heading.className = "header"
+
+const para = document.createElement("p");
+para.textContent = "Welcome to the Session"
+para.className = "para"
+
+const btn = document.createElement("button")
+btn.textContent = "Click Me"
+btn.className = "btn"
+
+div.append(heading)
+div.append(para)
+div.append(btn)
+
+document.getElementById("root").append(div)
