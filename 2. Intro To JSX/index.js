df6fe0e575ef07ec807e15776ed02c9008a20b27
Bikash <medhi01011997@gmail.com>
2024-08-02 13:14:15 +0530

    Module2: 4. Arrays in JSX

--- 2. Intro To JSX/index.js
+++ 2. Intro To JSX/index.js
@@ -1,11 +1,16 @@
 const App = () =>{
     const a = 6
     const b = 8
+    const list = ["apple","banana","orange"]
     const sum = (a,b) => a+b
     
     return (<>
        
         <h2>The sum of {a} and {b} is {sum(a,b)} </h2>
+        <h3>{list}</h3>
+        {list.map((item,index)=>{
+            return <p key={index}>{item}</p>
+        })}
     </>)
 }
 
